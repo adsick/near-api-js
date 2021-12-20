26791bba279fab41815bf31c640b84bb50c1cbf4
adsick <adsick@protonmail.com>
2021-12-20 16:29:00 +0200

    export provider

--- examples/cookbook/utils/check-account-existence.js
+++ examples/cookbook/utils/check-account-existence.js
@@ -1,17 +1,20 @@
 // Demonstrates checking if an account exists
 const { providers } = require("near-api-js");
 
-const provider = new providers.JsonRpcProvider(
+exports.provider = new providers.JsonRpcProvider(
     "https://archival-rpc.testnet.near.org"
 );
 
 async function accountExists() {
     let rawResult;
+    
+    console.log('DEBUG, current rpc_addr: ' + (await exports.provider.status()).rpc_addr);
+    console.log('DEBUG, current connection url: ' + exports.provider.connection.url);
 
     for (const account_id of ["does-not-exist.mike.testnet", "mike.testnet"]) {
         let succeeded = true;
         try {
-            rawResult = await provider.query({
+            rawResult = await exports.provider.query({
                 request_type: "view_account",
                 account_id: account_id,
                 finality: "final",
@@ -25,4 +28,4 @@ async function accountExists() {
     }
 }
 
-accountExists();
+exports.accountExists = accountExists;
