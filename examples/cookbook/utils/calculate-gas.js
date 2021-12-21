01036625e32cc1d2f69ecf3324d14066c2e42bc5
adsick <adsick@protonmail.com>
2021-12-21 16:51:58 +0200

    exporting stuff to make scripts testable

--- examples/cookbook/utils/calculate-gas.js
+++ examples/cookbook/utils/calculate-gas.js
@@ -3,38 +3,43 @@ const path = require("path");
 const homedir = require("os").homedir();
 const chalk = require("chalk");
 
-const CREDENTIALS_DIR = ".near-credentials";
-const ACCOUNT_ID = "near-example.testnet";
-const CONTRACT_ID = "guest-book.testnet";
-const METHOD_NAME = "addMessage";
-const MAX_GAS = "300000000000000";
-const ATTACHED_DEPOSIT = "0";
+exports.CREDENTIALS_DIR = ".near-credentials";
+exports.ACCOUNT_ID = "near-example.testnet";
+exports.CONTRACT_ID = "guest-book.testnet";
+exports.METHOD_NAME = "addMessage";
+exports.MAX_GAS = "300000000000000";
+exports.ATTACHED_DEPOSIT = "0";
 
 const args = {
   text: "Howdy!",
 };
 
-const credentialsPath = path.join(homedir, CREDENTIALS_DIR);
+const credentialsPath = path.join(homedir, exports.CREDENTIALS_DIR);
 const keyStore = new keyStores.UnencryptedFileSystemKeyStore(credentialsPath);
 
-const config = {
+exports.config = {
   keyStore,
   networkId: "testnet",
   nodeUrl: "https://rpc.testnet.near.org",
 };
 
-calculateGas(CONTRACT_ID, METHOD_NAME, args, ATTACHED_DEPOSIT);
+//usage
+//calculateGas(CONTRACT_ID, METHOD_NAME, args, ATTACHED_DEPOSIT);
 
 async function calculateGas(contractId, methodName, args, depositAmount) {
-  const near = await connect(config);
-  const account = await near.account(ACCOUNT_ID);
+  console.log('trying to connect with nodeUrl: ' + exports.config.nodeUrl)
+  const near = await connect(exports.config);
+  console.log('connected, getting ' + exports.ACCOUNT_ID + ' account')
+  const account = await near.account(exports.ACCOUNT_ID);
+  console.log('doing a function call')
   const result = await account.functionCall({
     contractId,
     methodName,
     args,
-    gas: MAX_GAS,
+    gas: exports.MAX_GAS,
     attachedDeposit: utils.format.parseNearAmount(depositAmount),
   });
+  console.log('done')
   const { totalGasBurned, totalTokensBurned } = result.receipts_outcome.reduce(
     (acc, receipt) => {
       acc.totalGasBurned += receipt.outcome.gas_burnt;
@@ -52,7 +57,7 @@ async function calculateGas(contractId, methodName, args, depositAmount) {
   );
 
   console.log(chalk`{white ------------------------------------------------------------------------ }`)
-  console.log(chalk`{bold.green RESULTS} {white for: [ {bold.blue ${METHOD_NAME}} ] called on contract: [ {bold.blue ${CONTRACT_ID}} ]}` )
+  console.log(chalk`{bold.green RESULTS} {white for: [ {bold.blue ${exports.METHOD_NAME}} ] called on contract: [ {bold.blue ${exports.CONTRACT_ID}} ]}` )
   console.log(chalk`{white ------------------------------------------------------------------------ }`)
   console.log(chalk`{bold.white Gas Burnt}     {white |}  {bold.yellow ${totalGasBurned}}`);
   console.log(chalk`{bold.white Tokens Burnt}  {white |}  {bold.yellow ${totalTokensBurned}}`);
@@ -63,3 +68,5 @@ async function calculateGas(contractId, methodName, args, depositAmount) {
     totalGasBurned,
   };
 }
+
+exports.calculateGas = calculateGas;
--- examples/cookbook/utils/check-account-existence.js
+++ examples/cookbook/utils/check-account-existence.js
@@ -5,27 +5,34 @@ exports.provider = new providers.JsonRpcProvider(
     "https://archival-rpc.testnet.near.org"
 );
 
-async function accountExists() {
+async function accountExists(account_id) {
     let rawResult;
     
-    console.log('DEBUG, current rpc_addr: ' + (await exports.provider.status()).rpc_addr);
-    console.log('DEBUG, current connection url: ' + exports.provider.connection.url);
+    let succeeded = true;
+    try {
+        rawResult = await exports.provider.query({
+            request_type: "view_account",
+            account_id: account_id,
+            finality: "final",
+        });
+    } catch (e) {
+        if (e.type === 'AccountDoesNotExist') {
+            succeeded = false;
+        }
+    }
+    return succeeded;
+}
+
+async function accountsExist() {
+    console.log('current connection url: ' + exports.provider.connection.url);
 
     for (const account_id of ["does-not-exist.mike.testnet", "mike.testnet"]) {
-        let succeeded = true;
-        try {
-            rawResult = await exports.provider.query({
-                request_type: "view_account",
-                account_id: account_id,
-                finality: "final",
-            });
-        } catch (e) {
-            if (e.type === 'AccountDoesNotExist') {
-                succeeded = false;
-            }
-        }
+        let succeeded = await accountExists(account_id);
         console.log(succeeded ? `The account ${account_id} exists.` : `There is no account ${account_id}.`)
     }
 }
 
+// accountsExist();
+
 exports.accountExists = accountExists;
+exports.accountsExist = accountsExist;
