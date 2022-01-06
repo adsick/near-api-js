e50b777305a1cc9013fcf6eb867d5f8dbacb3ffc
adsick <adsick@protonmail.com>
2022-01-06 14:25:12 +0200

    deploy contract

--- examples/cookbook/__tests__/deploy-contract.ava.ts
+++ examples/cookbook/__tests__/deploy-contract.ava.ts
@@ -1,27 +1,25 @@
+import { captureError } from 'near-workspaces';
 import { workspace } from './utils'
-import { providers, KeyPair } from 'near-api-js'
-const { path }  = require('path')
-import { keyStores } from 'near-workspaces/node_modules/near-api-js';
+
 
 workspace.test('deploy contract', async (test, { root, alice, status_message }) => {
     const config = alice["manager"]["config"];
 
     const module = require('../utils/deploy-contract')
-    
+    const keyStores = module.keyStores;
     let ks = new keyStores.InMemoryKeyStore();
     await ks.setKey('sandbox', alice.accountId, await alice.getKey());
 
-    //tvhnzbocwmqi3srun4jLpnJXGw5mHXynK3vpyCRrLpJmLfemRManztyv4VJepWpuC1Y6ftw4sQxWDUjPjQU7Nf1
-    //53y2QohkctpiwhQusvU2rEAtYTRxdyBYcBPV7C2AVyH5X9zs71q1g8UZhyWCn5bnjn3YaVTroR8LAVCPePTS6Ty7
-    const PRIVATE_KEY = "tvhnzbocwmqi3srun4jLpnJXGw5mHXynK3vpyCRrLpJmLfemRManztyv4VJepWpuC1Y6ftw4sQxWDUjPjQU7Nf1"
-    const keyPair = KeyPair.fromString(PRIVATE_KEY);
-    await ks.setKey('testnet', "adsick.testnet", keyPair);
-
     module.config.keyStore = ks
-    module.config.networkId = 'testnet'
+    module.config.networkId = 'sandbox'
     module.config.nodeUrl = config.rpcAddr;
     const { deployContract } = module
+    let result = await deployContract(alice.accountId, "__tests__/res/status_message.wasm");
+    test.truthy(result)
 
-    // test.truthy(await deployContract(alice.accountId, "__tests__/res/status_message.wasm"))
-    test.truthy(await deployContract("adsick.testnet", "__tests__/res/status_message.wasm"))
+    // now with wrong account id
+
+    let result2 = await captureError(() => deployContract(alice.accountId + 'a', "__tests__/res/status_message.wasm"));
+    console.log(result)
+    test.true(result.includes("Can not sign transactions for account"))
 })
--- examples/cookbook/utils/deploy-contract.js
+++ examples/cookbook/utils/deploy-contract.js
@@ -1,11 +1,10 @@
-const { keyStores, connect, Near, KeyPair } = require("near-api-js");
+const { keyStores, connect } = require("near-api-js");
 const fs = require("fs");
 const path = require("path");
-const { baseEncode } = require("borsh");
 const homedir = require("os").homedir();
 
 const CREDENTIALS_DIR = ".near-credentials";
-const ACCOUNT_ID = "near-example.testnet"; //your target here
+const ACCOUNT_ID = "near-example.testnet"; //your target account here
 const WASM_PATH = path.join(__dirname, "/wasm-files/status_message.wasm");
 const credentialsPath = path.join(homedir, CREDENTIALS_DIR);
 const keyStore = new keyStores.UnencryptedFileSystemKeyStore(credentialsPath);
@@ -19,44 +18,10 @@ exports.config = {
 // usage
 // deployContract(ACCOUNT_ID, WASM_PATH);
 
-// deployContract("adsick.testnet", WASM_PATH)
-
 async function deployContract(account_id, wasmPath) {
-    //debug, trying to find difference between key objects
-    console.log(await keyStore.getKey('testnet', 'adsick.testnet'))
-    // console.log(await exports.config.keyStore.getKey('sandbox', account_id))
-
-    // const account_id = "adsick.testnet";
-
-    // a 'normal' key store with which everything works fine
-    // const CREDENTIALS_DIR = ".near-credentials";
-    // const credentialsPath = path.join(homedir, CREDENTIALS_DIR);
-    // const keyStore = new keyStores.UnencryptedFileSystemKeyStore(credentialsPath);
-    
-    // let inMemoryKeyStore = new keyStores.InMemoryKeyStore();
-    // const PRIVATE_KEY = "tvhnzbocwmqi3srun4jLpnJXGw5mHXynK3vpyCRrLpJmLfemRManztyv4VJepWpuC1Y6ftw4sQxWDUjPjQU7Nf1"
-    // const keyPair = KeyPair.fromString(PRIVATE_KEY);
-    // await inMemoryKeyStore.setKey('testnet', account_id, keyPair);
-
-    // exports.config.keyStore = inMemoryKeyStore;
-
     const near = await connect(exports.config);
-    // console.log(near, '\n')
     const account = await near.account(account_id);
-    // console.log(account, '\n')
 
-    // example of signing some message (works both times)
-    let sign = await account.connection.signer.signMessage(
-        "message",
-        account_id,
-        exports.config.networkId);
-    
-    // ensure that there is a proper key.
-    console.log('public key: ', sign.publicKey);
-    console.log('debug: ', account.connection.signer);
-
-
-    // fails with 'no key found...' error
     const result = await account.deployContract(fs.readFileSync(wasmPath));
 
     console.log(result);
@@ -65,13 +30,4 @@ async function deployContract(account_id, wasmPath) {
 }
 
 exports.deployContract = deployContract;
-
-    /*  
-    Error (BorshError) {
-        fieldPath: [
-            'publicKey',
-          ],
-          originalMessage: 'Class PublicKey is missing in schema',
-          message: 'Class PublicKey is missing in schema: publicKey',
-        } 
-    */
+exports.keyStores = keyStores;
