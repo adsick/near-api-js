170d9f937fa78b88f8b31d4b9093094caf02354c
adsick <adsick@protonmail.com>
2022-01-10 16:59:05 +0200

    deploy linkdrop.test.near

--- examples/cookbook/__tests__/utils.ts
+++ examples/cookbook/__tests__/utils.ts
@@ -7,10 +7,11 @@ async function initWorkspace(root: NearAccount) {
 
     const alice = await root.createAccount('alice');
     const status_message = await root.createAndDeploy('status_message', '__tests__/res/status_message.wasm');
+    const linkdrop = await root.createAndDeploy('linkdrop', '__tests__/res/linkdrop.wasm');
     // console.log('current mainnet rpc address is: ', MAINNET_RPC_ADDR) //note, no these for 'sandbox'
     // console.log('current testnet rpc address is: ', TESTNET_RPC_ADDR)
     
-    return { alice, status_message};
+    return { alice, status_message, linkdrop };
 }
 
 export const STORAGE_PER_BYTE = new BN('10000000000000000000');
